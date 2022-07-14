af27cda2688b2b4fb257eea15220d7570e92efd0
dododo04 <108640429+dododo04@users.noreply.github.com>
2022-07-14 10:51:35 +0900

    ㅇㅇ

--- jeogiyo (1)/jeogiyo/src/main/java/com/jpro/jcontroller/JConsumerController.java
+++ jeogiyo (1)/jeogiyo/src/main/java/com/jpro/jcontroller/JConsumerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class JConsumerController {
 
-	//메인페이지
+	//메인페이지 메인
 	@RequestMapping("consumer")
 	public ModelAndView order() {
 		ModelAndView mv = new ModelAndView();
